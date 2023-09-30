fb50155dd3dbc168a97f9f33bbb1651b2563d176
RuiTakao <ruia1082halfnc@gmail.com>
2023-10-01 01:04:58 +0900

    必要ファイル作成

---
+++ src/Model/Entity/Post.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Model\Entity;
+
+use Cake\ORM\Entity;
+
+/**
+ * Post Entity
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property string $image_path
+ * @property bool $image_flg
+ * @property string $url
+ * @property bool $url_flg
+ * @property \Cake\I18n\FrozenTime $created
+ * @property \Cake\I18n\FrozenTime $modified
+ */
+class Post extends Entity
+{
+    /**
+     * Fields that can be mass assigned using newEntity() or patchEntity().
+     *
+     * Note that when '*' is set to true, this allows all unspecified fields to
+     * be mass assigned. For security purposes, it is advised to set '*' to false
+     * (or remove it), and explicitly make individual fields accessible as needed.
+     *
+     * @var array<string, bool>
+     */
+    protected $_accessible = [
+        'title' => true,
+        'body' => true,
+        'image_path' => true,
+        'image_flg' => true,
+        'url' => true,
+        'url_flg' => true,
+        'created' => true,
+        'modified' => true,
+    ];
+}
---
+++ tests/Fixture/PostsFixture.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Test\Fixture;
+
+use Cake\TestSuite\Fixture\TestFixture;
+
+/**
+ * PostsFixture
+ */
+class PostsFixture extends TestFixture
+{
+    /**
+     * Init method
+     *
+     * @return void
+     */
+    public function init(): void
+    {
+        $this->records = [
+            [
+                'id' => 1,
+                'title' => 'Lorem ipsum dolor sit amet',
+                'body' => 'Lorem ipsum dolor sit amet, aliquet feugiat. Convallis morbi fringilla gravida, phasellus feugiat dapibus velit nunc, pulvinar eget sollicitudin venenatis cum nullam, vivamus ut a sed, mollitia lectus. Nulla vestibulum massa neque ut et, id hendrerit sit, feugiat in taciti enim proin nibh, tempor dignissim, rhoncus duis vestibulum nunc mattis convallis.',
+                'image_path' => 'Lorem ipsum dolor sit amet',
+                'image_flg' => 1,
+                'url' => 'Lorem ipsum dolor sit amet',
+                'url_flg' => 1,
+                'created' => '2023-10-01 00:48:10',
+                'modified' => '2023-10-01 00:48:10',
+            ],
+        ];
+        parent::init();
+    }
+}
