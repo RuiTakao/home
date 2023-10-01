d03ca1f0054e52455a4d0ef223c4825d854496ff
RuiTakao <ruia1082halfnc@gmail.com>
2023-10-01 18:12:55 +0900

    投稿フォーム修正

--- templates/Admin/Posts/add.php
+++ templates/Admin/Posts/add.php
@@ -1,38 +1,92 @@
 <?php
+
 /**
  * @var \App\View\AppView $this
  * @var \App\Model\Entity\Post $post
  */
+$this->Form->setTemplates([
+    'inputContainer' => '{{content}}'
+])
 ?>
-<div class="row">
-    <aside class="column">
-        <div class="side-nav">
-            <h4 class="heading"><?= __('Actions') ?></h4>
-            <?= $this->Html->link(__('List Posts'), ['action' => 'index'], ['class' => 'side-nav-item']) ?>
-        </div>
-    </aside>
-    <div class="column-responsive column-80">
-        <div class="posts form content">
-            <?= $this->Form->create($post, [
-                'type' => 'file'
-            ]) ?>
-            <fieldset>
-                <legend><?= __('Add Post') ?></legend>
-                <?php
-                    echo $this->Form->control('title');
-                    echo $this->Form->control('body',[
-                        'type' => 'textarea'
-                    ]);
-                    echo $this->Form->control('image_path',[
-                        'type' => 'file'
-                    ]);
-                    echo $this->Form->control('image_flg');
-                    echo $this->Form->control('url');
-                    echo $this->Form->control('url_flg');
-                ?>
-            </fieldset>
-            <?= $this->Form->button(__('Submit')) ?>
-            <?= $this->Form->end() ?>
-        </div>
+<div class="lists">
+    <div class="list list_add" style="display:block;">
+        <?= $this->Form->create($post, [
+            'type' => 'file'
+        ]) ?>
+        <ul class="form_lists">
+            <li class="form_list">
+                <div class="form_list_title">
+                    <label for="title">作品名：</label>
+                </div>
+                <div class="form_list_input">
+                    <?= $this->Form->control('title', [
+                        'id' => 'title',
+                        'label' => false,
+                        'required' => false
+                    ]) ?>
+                </div>
+            </li>
+            <li class="form_list">
+                <div class="form_list_title">
+                    <label for="body">説明：</label>
+                </div>
+                <div class="form_list_input">
+                    <?= $this->Form->control('body', [
+                        'id' => 'body',
+                        'type' => 'textarea',
+                        'label' => false,
+                        'required' => false
+                    ]) ?>
+                </div>
+            </li>
+            <li class="form_list">
+                <div class="form_list_title">
+                    <label for="status">ステータス：</label>
+                </div>
+                <div class="form_list_input">
+                    <?= $this->Form->radio('status', [
+                        ['value' => 1, 'text' => '表示'],
+                        ['value' => 0, 'text' => '非表示']
+                    ]) ?>
+                </div>
+            </li>
+            <li class="form_list">
+                <div class="form_list_title">
+                    <label for="url">URL：</label>
+                </div>
+                <div class="form_list_input">
+                    <?= $this->Form->control('url', [
+                        'id' => 'url',
+                        'label' => false,
+                        'required' => false
+                    ]) ?>
+                    <?= $this->Form->checkbox('url_flg', [
+                        'id' => 'url_flg'
+                    ]) ?><label for="url_flg">なし</label>
+                </div>
+            </li>
+            <li class="form_list">
+                <div class="form_list_title">
+                    <label for="url">画像名：</label>
+                </div>
+                <div class="form_list_input">
+                    <?= $this->Form->control('image_name', [
+                        'id' => 'image_name',
+                        'label' => false,
+                        'required' => false
+                    ]) ?>
+                    <?= $this->Form->checkbox('image_flg', [
+                        'id' => 'image_flg'
+                    ]) ?><label for="image_flg">なし</label>
+                    <?= $this->Form->control('image_path', [
+                        'type' => 'file',
+                        'label' => false,
+                        'required' => false
+                    ]) ?>
+                </div>
+            </li>
+        </ul>
+        <?= $this->Form->button(__('Submit')) ?>
+        <?= $this->Form->end() ?>
     </div>
 </div>
--- templates/Admin/Posts/index.php
+++ templates/Admin/Posts/index.php
@@ -1,15 +1,15 @@
 <ul class="lists">
     <?php foreach ($posts as $post) : ?>
-        <li class="list">
-            <div class="list_image">
+        <li class="list list_add">
+            <div class="list_image list_add_image">
                 <?= $this->Html->image(h($post->image_path), ['class' => 'img', 'alt' => 'CakePHP']) ?>
             </div>
             <div class="list_text">
-                <p class="list_title"><?= h($post->title) ?></p>
-                <p class="list_title">画像名： <?= h($post->title) ?></p>
-                <p>URL: <?= $this->Html->link(h($post->url), h($post->url), ['target' => '_blank']) ?></p>
+                <p class="list_title"><?= h($post->title) ?><span class="list_add_status status_true">表示</span></p>
+                <p class="list_sub_title mt8"><span class="weight">画像名：</span> <?= h($post->title) ?></p>
+                <p class="list_sub_title"><span class="weight">URL：</span> <?= $this->Html->link(h($post->url), h($post->url), ['target' => '_blank']) ?></p>
                 <p class="list_body"><?= h($post->body) ?></p>
-                <?= $this->Html->link('編集する', ['controller' => 'posts', 'action' => 'edit', $post->id], ['class' => 'list_link']) ?>
+                <?= $this->Html->link('編集する', ['controller' => 'posts', 'action' => 'edit', $post->id], ['class' => 'list_link list_add_link']) ?>
             </div>
         </li>
     <?php endforeach; ?>
